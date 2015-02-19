0d6e897024079d34815f60bf537981ad0de01300
Joaquin Fernandez <joaquinfndz@gmail.com>
2015-02-19 01:14:26 -0800

    headers fix

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,9 @@
 class Api::BaseController < ApplicationController
   respond_to :json, :xml
-  headers['Access-Control-Allow-Origin'] = '*'
-  headers['Access-Control-Allow-Methods'] = 'GET'
+  before_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+  end
 end
